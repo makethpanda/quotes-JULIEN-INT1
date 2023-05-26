37970596adc94b48ad1d36040aa3ca13c70089ed
Val <valgebo@gmail.com>
2023-05-26 08:42:44 +0200

    add an add_quote() function

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def menu():
     print("1. Random quote")
     print("2. All quotes")
     print("3. Exit")
+    print("4. Add a quote")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input("Choose your an action (1-4): ")
         
         if choice == "1":
             print_quote(random_quote(quotes))
@@ -20,6 +21,8 @@ def main():
         elif choice == "3":
             print("Good bye...")
             break
+        elif choice == "4":
+            add_quote(quotes,"quotes.txt")
         else:
             print("Invalid input")
 
